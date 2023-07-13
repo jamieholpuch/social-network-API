85965ee758c225cb92c42755c5e4738ec32e92ba
jamieholpuch <125837353+jamieholpuch@users.noreply.github.com>
2023-07-12 21:51:53 -0400

    created models

---
+++ models/Reaction.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: new ObjectId 
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxLength: 280,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now, 
+        get: Date.format(),
+    },
+});
+
+
+module.exports = reactionSchema;
